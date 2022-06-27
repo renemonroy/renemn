4b5ce7454db9f95f8fbeff97b278bf96222f4415
René Monroy <rene.monroy@gmail.com>
2022-06-27 00:16:11 -0500

    Configure tailwindcss

---
+++ apps/rene.mn/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        "open-sans": ['"Open Sans"', "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
