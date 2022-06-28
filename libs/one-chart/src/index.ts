e58ec85653ef54f63653a22a546b6aa176586500
René Monroy <rene.monroy@gmail.com>
2022-06-28 15:30:24 -0500

    Export required components from One Chart library

--- libs/one-chart/src/index.ts
+++ libs/one-chart/src/index.ts
@@ -1,1 +1,7 @@
-export * from "./lib/OneChart";
+import OneChart from "./lib/OneChart";
+import { ChartsProvider as chartsProvider } from "./stores/ChartsStore/ChartsStore";
+import useChartHook from "./hooks/useChart/useChart";
+
+export const ChartsProvider = chartsProvider;
+export const useChart = useChartHook;
+export default OneChart;
